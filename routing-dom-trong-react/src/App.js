4e55a6e9c5d34602c61f0b67561ec701ca72fc50
nhando <trongnhan19081994@gmail.com>
2021-04-26 17:38:40 +0700

    update

--- routing-dom-trong-react/src/App.js
+++ routing-dom-trong-react/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import productApi from './api/productApi';
 import NotFound from './components/NotFound';
 import AlbumFearure from './features/Album';
 import TodoFeature from './features/Todo';
 function App() {
+
+  useEffect(()=> {
+      const fetchProducts = async () => {
+        const params = {
+          _limit: 10,
+        }
+        const productList = await productApi.getAll(params);
+        console.log(productList);
+      }
+      fetchProducts();
+  }, [])
+
   return (
     <div className="App">
       Header
