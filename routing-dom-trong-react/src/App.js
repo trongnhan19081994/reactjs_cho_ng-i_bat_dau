cb06a1296f34e8a5594f5308027874df19ec9203
Nhan Do <trongnhan19081994@gmail.com>
2021-05-06 22:11:59 +0700

    update

--- routing-dom-trong-react/src/App.js
+++ routing-dom-trong-react/src/App.js
@@ -1,5 +1,6 @@
+import Header from 'components/Header';
 import { useEffect } from 'react';
-import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import productApi from './api/productApi';
 import NotFound from './components/NotFound';
 import AlbumFearure from './features/Album';
@@ -20,13 +21,15 @@ function App() {
 
   return (
     <div className="App">
-      Header
-      <p>
+
+      <Header />
+
+      {/* <p>
         <NavLink to='/todos' activeClassName='active-menu'>Todos</NavLink>
       </p>
       <p>
         <NavLink to='/albums'>Albums</NavLink>
-      </p>
+      </p> */}
       <Switch>
         <Redirect from='/home' to='/' exact />
         <Redirect from='/post-list/:postId' to='/posts/:postId' exact />
@@ -35,7 +38,7 @@ function App() {
         {/* <Route path='/' component={TodoFeature} exact /> */}
         <Route path='/todos' component={TodoFeature} />
         <Route path='/albums' component={AlbumFearure} />
-        {/* <Route component={NotFound} /> */}
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
--- routing-dom-trong-react/src/components/Header/index.jsx
+++ routing-dom-trong-react/src/components/Header/index.jsx
@@ -1,20 +1,85 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './styles.scss';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CodeIcon from '@material-ui/icons/Code';
+import { Link, NavLink } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { useState } from 'react';
 
-Header.propTypes = {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+      textDecoration: 'none',
+      color: 'white'
+  }
+}));
 
+export default function Header() {
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <CodeIcon className={classes.menuButton}/>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" className={classes.link}>  EZ SHOP </Link>
+          </Typography>
+            <NavLink to='/todos' activeClassName='active-menu' className={classes.link}>
+                <Button color="inherit">Todos</Button>
+            </NavLink>
+            <NavLink to='/albums' className={classes.link}>
+                <Button color="inherit">Albums</Button>
+            </NavLink>
+          <Button color="inherit" onClick={handleClickOpen}>Register</Button>
+        </Toolbar>
+      </AppBar>
+      <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
-
-function Header(props) {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-
-
-export default Header
-
